27c7cbcb0dde8c71ca8030a13cffe8da278230c7
kamilhayat <kamilhayat27@gmail.com>
2024-10-04 16:45:53 +0530

    final submit

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json()); 
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/itemsdb', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/itemsdb',)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
